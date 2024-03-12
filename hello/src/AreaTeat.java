51d4ed83b65ddf9d99852bb95464090d811fe841
leetaeyong <tkwk651@naver.com>
2024-03-12 10:53:36 +0900

    AreaTeat1

---
+++ hello/src/AreaTeat.java
@@ -0,0 +1,13 @@
+
+public class AreaTeat {
+
+	public static void main(String[] args) {
+		
+		double radius, area;
+		radius = 3.0;
+		area = radius* radius * 3.141592;
+		
+		System.out.println("반지름이"+radius+"인 원의 면적은"+area+"입니다.");
+		//pritn에 ln을 붙이면 기본적으로 줄바꿈이 들어간다
+	}
+}
