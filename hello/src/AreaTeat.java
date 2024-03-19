f34d037f04c846a3cfc572a99ded5e296202e393
leetaeyong <tkwk651@naver.com>
2024-03-19 09:32:47 +0900

    11

--- hello/src/AreaTeat.java
+++ hello/src/AreaTeat.java
@@ -1,11 +1,17 @@
+import java.util.Scanner;
 
 public class AreaTeat {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+		
 		final double PI = 3.141592;
 		double radius, area;
-		radius = 3.0;
+		
+		System.out.print("첫번째 숫자를 입력하시오: ");
+		radius= input.nextInt();
+		
 		area = radius* radius * PI;
 		
 		System.out.println("반지름이"+radius+"인 원의 면적은"+area+"입니다.");
